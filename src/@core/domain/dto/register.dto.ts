97013a4d07e0d3817ac4d996559238eec778f47c
Ricardo Tadeu Sperlongo <ricardo_tadeu30@hotmail.com>
2023-09-27 14:09:12 -0300

    create database table user with roles

---
+++ src/@core/domain/dto/register.dto.ts
@@ -0,0 +1,23 @@
+import { IsEnum, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import Role from '../enum/role.enum';
+
+export class RegisterDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+
+  @IsNotEmpty()
+  @IsEnum(Role)
+  type: Role;
+}
+
+export default RegisterDto;
