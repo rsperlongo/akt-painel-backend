bf0bc8edd33b375d03c1af78d5b12097d5e116fe
Ricardo Tadeu Sperlongo <ricardo_tadeu30@hotmail.com>
2023-10-30 15:19:55 -0300

    update api endpoint

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalGuards();
+  app.setGlobalPrefix('api')
   app.enableCors({
     origin: [
       'https://sistema-boleto-server-production.up.railway.app',
--- src/users/role.guard.ts
+++ src/users/role.guard.ts
@@ -1,28 +1,5 @@
-// import { Injectable, CanActivate, ExecutionContext, Type, mixin } from '@nestjs/common';
-// import { Reflector } from '@nestjs/core';
-// import Role from 'src/@core/domain/enum/role.enum';
-// import RequestWithUser from 'src/auth/interfaces/requestWithUser.interface';
-// import JwtAuthenticationGuard from 'src/auth/jwt-authentication.guard';
-
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
-
-//   const RoleGuard = (role: Role): Type<CanActivate> => {
-//     class RoleGuardMixin extends JwtAuthenticationGuard {
-//       async canActivate(context: ExecutionContext) {
-//         await super.canActivate(context);
-   
-//         const request = context.switchToHttp().getRequest<RequestWithUser>();
-//         const user = request.user;
-   
-//         return user?.roles.includes(role);
-//       }
-//     }
-   
-//     return mixin(RoleGuardMixin);
-//   }
-   
-// export default RoleGuard;
 
 
 @Injectable()
