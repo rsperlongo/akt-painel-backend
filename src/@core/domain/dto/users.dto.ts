f7a3ca11ba2d7d7bd5655bb8ca1cf04417f947ca
Ricardo Tadeu Sperlongo <ricardo_tadeu30@hotmail.com>
2023-11-04 17:50:53 -0300

    latest comits

--- src/@core/domain/dto/users.dto.ts
+++ src/@core/domain/dto/users.dto.ts
@@ -1,11 +1,30 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import Role from '../enum/role.enum';
 export class UserDto {
+  @IsString()
   @IsNotEmpty()
   id: string;
 
-  @IsNotEmpty()
   @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
   username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  finalNumber: string;
+
+  @IsNotEmpty()
+  @IsEnum(Role)
+  roles: Role;
 
   createdOn?: Date;
 }
--- src/shared/mapper.ts
+++ src/shared/mapper.ts
@@ -2,11 +2,15 @@ import { UserDto } from 'src/@core/domain/dto/Users.dto';
 import { User } from 'src/@core/domain/entities/user.entity';
 
 export const toUserDto = (data: User): UserDto => {
-  const { id, username } = data;
+  const { id, name, username, finalNumber, password, roles } = data;
 
   const usersDto : UserDto = {
     id,
+    name,
     username,
+    finalNumber,
+    password,
+    roles
   };
 
   return usersDto;
