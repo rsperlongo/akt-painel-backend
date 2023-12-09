2743eccc2c89b9ad0c8c0b710be3646daf72584a
Ricardo Tadeu Sperlongo <ricardo_tadeu30@hotmail.com>
2023-12-09 08:04:58 -0300

    last changes

--- src/@core/application/use-cases/pix.use-case.ts
+++ src/@core/application/use-cases/pix.use-case.ts
@@ -1,13 +1,9 @@
 import { HttpService } from '@nestjs/axios';
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { AuthGuard } from '@nestjs/passport';
 import { InjectRepository } from "@nestjs/typeorm";
 import { AxiosResponse } from 'axios';
 import { Observable, map } from 'rxjs';
 import { Pix } from "src/@core/domain/entities/pix.entiy";
-import { Repository } from "typeorm";
-const express = require('express')
-const app = express()
 
 @Injectable()
 export class PixService {
@@ -15,6 +11,11 @@ export class PixService {
         @InjectRepository(Pix)
         private readonly httpService: HttpService
     ) { }
+
+    createPix(nome: string, cidade: string, chave: string, valor: string): Observable<AxiosResponse<any>> {
+        return this.httpService.get<any>(`https://gerarqrcodepix.com.br/api/v1?nome=${nome}&cidade=${cidade}&saida=qr&chave=${chave}&valor=${valor}` ,
+       ).pipe(map((resp) => resp.data))
+    }
 
 
 }
